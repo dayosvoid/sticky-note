e84d5e37039c4dbfe73dbfc037af007ea9d49334
Adedayo <atimileyin911@gmail.com>
2026-05-02 18:06:33 +0100

    .

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,19 +1,12 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL:'/api'
+    baseURL: import.meta.env.VITE_API_URL 
+        ? import.meta.env.VITE_API_URL + '/api'
+        : '/api' // fallback to proxy during development
 })
 
 axiosInstance.interceptors.request.use(
-    // (config)=>{
-    //     const token = localStorage.getItem(token)
-    //     if(token){
-    //         config.headers.Authorization = `Bearer ${token}`
-    //     }
-    //     return config
-    // },
-    // (error) => {
-    //     return Promise.reject(error);
-    // }
+
 )
 
